e5c31db6f2d06c3d129dba61da3cfd5bd0cc7b94
estacimo <w0825062@myscc.ca>
2023-10-30 19:53:42 -0400

    Recreated the new projects

---
+++ EstasBookStore.Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace EstasBookStore.Models
+{
+    public class Class1
+    {
+    }
+}
---
+++ EstasBookStore.Utility/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace EstasBookStore.Utility
+{
+    public class Class1
+    {
+    }
+}
